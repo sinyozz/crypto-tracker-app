c5797ed65c74057a73cd01db91dd0327ecc218ac
Siny Christensen <sinychristensen@Sinys-MacBook-Pro.local>
2021-07-09 18:09:44 -0700

    mobile responsive view fixed

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ const [search, setSearch] = useState('')
 
 
 useEffect(() => {
-  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
 .then(res => {
   setCoins(res.data);
   
 // console.log(res.data)
 
-}). catch(error => alert ('yoo error'));
+}).catch(error => alert ('yoo error'));
 }, []);
 
 const handleChange = e =>{
@@ -51,9 +51,7 @@ const filterCoins= coins.filter (coin => coin.name.toLowerCase().includes(search
         />
       )})}
 
-<div className='copyrights'>
-  <p>by Siny C. </p>
-</div>
+
     </div>
   );
 }
